168b9e3ab0ebb96d72ba42e1423ab13bfbcde0b8
Ega Faradhia Yogachi <egafaradhiayogachi@gmail.com>
2023-02-18 12:11:05 +0700

    add build

---
+++ .next/static/fsduFVFn33hBYIeGJbQRp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fcss\u002F8ab728bc1dd9cce5.css","static\u002Fchunks\u002Fpages\u002Findex-f2818c1345682668.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
